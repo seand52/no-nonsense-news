d4e2ce2b0e06d1f977907dd17fe52e6a6bfd10b0
Sean <seand52@gmail.com>
2021-04-05 19:05:14 +0200

    Add  fetch data from guardian api

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"text/template"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // detail structs
@@ -54,22 +57,37 @@ type Result struct {
 	Type               string `json:"type"`
 	WebPublicationDate string `json:"webPublicationDate"`
 	WebTitle           string `json:"webTitle"`
-	Slug           string `json:"slug"`
+	ApiUrl             string `json:"apiUrl"`
+}
+
+type FilteredResult struct {
+	Id                 string `json:"id"`
+	Type               string `json:"type"`
+	WebPublicationDate string `json:"webPublicationDate"`
+	WebTitle           string `json:"webTitle"`
+	Slug               string `json:"slug"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	jsonFile, err := os.Open("overview.json")
+	jsonFile, err := os.Open("test.json")
 	if err != nil {
 		//hondle the error
 	}
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-
+	re := regexp.MustCompile(`([^\/]+$)`)
 	var news ResponseOverview
 	json.Unmarshal(byteValue, &news)
-	var filteredNews []Result
+	var filteredNews []FilteredResult
 	for _, v := range news.Response.Result {
 		if v.Type != "liveblog" {
-			filteredNews = append(filteredNews, v)
+			id := string(re.Find([]byte(v.Id)))
+			var test FilteredResult
+			test.Id = v.Id
+			test.Type = v.Type
+			test.WebPublicationDate = v.WebPublicationDate
+			test.WebTitle = v.WebTitle
+			test.Slug = id
+			filteredNews = append(filteredNews, test)
 		}
 	}
 	t, err := template.ParseFiles("edit.html")
@@ -77,33 +95,89 @@ func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		//hondle the error
 		fmt.Println(err)
 	}
-  fmt.Println(filteredNews)
 	err = t.Execute(w, filteredNews)
 	if err != nil {
 		//hondle the error
 		fmt.Println(err)
 	}
+	resp, err := http.Get("https://content.guardianapis.com/search?api-key=a3136ec7-05ca-42c8-b0ac-60f9eae61e85&page-size=3")
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+	// body, err := ioutil.ReadAll(resp.Body)
+	var apiResult map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&apiResult)
+	fmt.Println(apiResult)
+
+	file, _ := json.MarshalIndent(apiResult, "", " ")
+	_ = ioutil.WriteFile("data/test2.json", file, 0644)
+}
+
+func fetchNewsdetail() {
+	jsonFile, err := os.Open("test.json")
+	if err != nil {
+		//hondle the error
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var news ResponseOverview
+	json.Unmarshal(byteValue, &news)
+	for _, v := range news.Response.Result {
+		if v.Type != "liveblog" {
+			apiUrl := v.ApiUrl + "?api-key=a3136ec7-05ca-42c8-b0ac-60f9eae61e85&show-blocks=all"
+			fmt.Println("============================== before request")
+			resp, err := http.Get(apiUrl)
+			if err != nil {
+				// handle error
+			}
+			defer resp.Body.Close()
+			// body, err := ioutil.ReadAll(resp.Body)
+			var apiResult map[string]interface{}
+			json.NewDecoder(resp.Body).Decode(&apiResult)
+			file, _ := json.MarshalIndent(apiResult, "", " ")
+			re := regexp.MustCompile(`([^\/]+$)`)
+			slug := string(re.Find([]byte(v.Id)))
+			err = ioutil.WriteFile("data/"+slug+".json", file, 0644)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("============================== before sleep")
+      time.Sleep(2*time.Second)
+			fmt.Println("============================== after sleep")
+
+		}
+	}
 
 }
 
 func renderNewsDetail(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	jsonFile, _ := os.Open("response.json")
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	fetchNewsdetail()
+	jsonFile, err := os.Open("response.json")
+	if err != nil {
+		//hondle the error
+	}
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		//hondle the error
+	}
 	var newsDetail Response
 	json.Unmarshal(byteValue, &newsDetail)
 	fmt.Println(newsDetail)
 
 	articleId := p.ByName("articleId")
 	fmt.Println(articleId)
-	t, _ := template.ParseFiles("newsDetail.html")
+	t, err := template.ParseFiles("newsDetail.html")
+	if err != nil {
+		//hondle the error
+	}
 	t.Execute(w, newsDetail)
-
 }
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", handler)
 	router.GET("/article/:articleId", renderNewsDetail)
-	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-  router.ServeFiles("/static/*filepath", http.Dir("static"))
+	router.ServeFiles("/static/*filepath", http.Dir("static"))
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
